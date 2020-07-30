3304ac8ab8b6d4d32c12afcfb31edd316cc19e57
Hans-Henrik Høgsted <hh.hoegsted@gmail.com>
2020-07-30 10:11:13 +0200

    FEATURE: First iteration of a solution

--- README.md
+++ README.md
@@ -7,6 +7,12 @@
 
 The objective of this challenge is to solidify what you've learn't in the Dicee tutorial. This app will guide you through the steps required to build an 8 ball app and you will be applying what you have learnt about Stateless and Stateful Flutter widgets.
 
+## My solution
+
+This was really simple - though I learned you have to have a Flex container around an Expanded widget - for example a Row or a Column.
+
+- HH
+
 
 ## What you will create
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: const Color(0xFF718792),
+          appBar: AppBar(
+            backgroundColor: const Color(0xFF455A64),
+            title: Text('Magic 8-Ball'),
+          ),
+          body: EightBall(),
+        ),
       ),
     );
+
+class EightBall extends StatefulWidget {
+  @override
+  _EightBallState createState() => _EightBallState();
+}
+
+class _EightBallState extends State<EightBall> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      ballNumber = Random().nextInt(5) + 1;
+                    });
+                  },
+                  child: Image.asset('images/ball$ballNumber.png')),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
